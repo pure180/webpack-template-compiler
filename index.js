9a0342052fea486b0e3425871110376196f7d515
Daniel Pfisterer <info@daniel-pfisterer.de>
2018-02-19 19:13:23 +0100

    Fix lint errors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+/*jshint esversion: 6 */
+/* jshint node: true */
+
 'use strict';
 
-const buildProduction   = require('./scripts/build');
-const startServer       = require('./scripts/start.js');
+const buildProduction = require('./scripts/build'),
+      nodemon = require('nodemon'),
+      startServer = require('./scripts/start.js');
 
-const argv              = process.argv;
-const production        = argv.indexOf('--prod') > -1;
-const start             = argv.indexOf('--serve') > -1;
+
+const argv = process.argv;
+const production = argv.indexOf('--prod') > -1;
+const start = argv.indexOf('--serve') > -1;
 
 if (production) {
   buildProduction();
