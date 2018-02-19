c170dd5dde41d9658a99007d7ca36f74deb89874
Daniel Pfisterer <info@daniel-pfisterer.de>
2018-02-19 19:23:24 +0100

    Create glob for json files and setup watch options

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -11,18 +11,35 @@ const paths = require('./paths');
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
 
+const glob = require('glob');
+const contentBaseGlob = glob.sync(paths.appPublic + '/**/*');
+const filterContentBase = (filter, reg) => {
+  const arr = [];
+  const regEx = reg ? reg : /\.[A-Za-z]*$/;
+  filter.forEach(n => {
+    if (!regEx.test(n)) {
+      arr.push(n);
+    }
+  });
+  return arr;
+}
+
+const contentBase = filterContentBase(contentBaseGlob)
+
 module.exports = function(proxy, allowedHost) {
   return {
     disableHostCheck:
       !proxy || process.env.DANGEROUSLY_DISABLE_HOST_CHECK === 'true',
     compress: true,
     clientLogLevel: 'none',
-    contentBase: paths.appDist,
+    contentBase: contentBase,
     watchContentBase: true,
     hot: true,
     publicPath: config.output.publicPath,
     quiet: true,
     watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
       ignored: /node_modules/,
     },
     https: protocol === 'https',
